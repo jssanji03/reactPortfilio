f0b97c1f2227abc0aef180b85b0d353333d8a32b
jess <jesssung@soetek.com.tw>
2022-08-01 20:25:11 +0800

    HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import "./assets/all.scss"
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,9 +9,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App/> */}
-    <BrowserRouter>
+    <HashRouter>
       <App> 在index.js中設定文字 </App>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
