19d68a3fee3137c22b5584273d6fd29b2c9fff88
jess <jesssung@soetek.com.tw>
2022-08-09 13:34:03 +0800

    add package.json homepage

--- src/pages/CareFlyer/CareFlyer.js
+++ src/pages/CareFlyer/CareFlyer.js
@@ -2,7 +2,8 @@ import React, { useState,useEffect } from 'react';
 import axios from 'axios';
 import './CareFlyer.scss';
 
-const SecondPage=()=>{
+const SecondPage = () => {
+   const[getData,setGetData]=useState(null)
     const StyleSheet={
         width:"100vw",
         height:"100vh",
@@ -13,35 +14,45 @@ const SecondPage=()=>{
         flexDirection:"column"
     }
       // -------- 取得目前user的資料 ---------- //
-    // function getUserInfoFromServer() {
-    //     const myHeaders = new Headers();
-    //     myHeaders.append("Authorization", "Client-ID {{clientId}}");
-    //     const requestOptions = {
-    //         method: 'GET',
-    //         headers: {
-    //             Authorization:'75b19c3170301ac'
-    //         },
-    //         // body: formdata,
-    //         redirect: 'follow'
-    //     };
-    //     fetch("https://api.imgur.com/3/album/oRuzdkT", requestOptions)
-    //         .then(response => response.text())
-    //         .then(result => console.log(result))
-    //         .catch(error => console.log('error', error));
-
-    // }
-    useEffect(() => {
-    // getUserInfoFromServer();
-    const headers = {
+    function getUserInfoFromServer() {
+        const myHeaders = new Headers();
+        myHeaders.append("Authorization", "Client-ID {{clientId}}");
+        const requestOptions = {
+            method: 'GET',
             headers: {
                 Authorization:'75b19c3170301ac'
             },
+            // body: formdata,
             redirect: 'follow'
         };
-    axios.get('https://api.imgur.com/3/album/oRuzdkT',{headers})
-        .then(result => console.log(result));
+        fetch("https://api.imgur.com/3/album/oRuzdkT", requestOptions)
+          .then(response => {
+            response.json();
+          })
+          .then(result => {
+            console.log(result);
+            setGetData(result.data);
+            console.log(getData);
+          })
+            .catch(error => console.log('error', error));
 
-  },[]);
+    }
+  
+  useEffect(() => {
+    getUserInfoFromServer();
+    // const headers = {
+    //         headers: {
+    //             Authorization:'75b19c3170301ac'
+    //         },
+    //         redirect: 'follow'
+    //     };
+    // axios.get('https://api.imgur.com/3/album/oRuzdkT', headers)
+    //   .then(response => {
+    //     setGetData(response.data);
+    //     console.log(getData);
+    //     console.log(response.data.images);
+    //   })
+  });
   
     return(
         <div style={StyleSheet}>
